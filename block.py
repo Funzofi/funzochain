c1174869a9d420d961801408e49e890b9922f4d5
Sushant Shah <sushantshah@cyrocoders.dev>
2022-05-22 17:51:45 +0530

    Create block.py

---
+++ block.py
@@ -0,0 +1,23 @@
+from cmath import pi
+from imports import *
+
+class block(object):
+    def __init__(self, node, data, seed=b"", validators=[]):
+        self.creator = node.address
+        self.data = data
+        self.timestamp = time.time()
+        self.seed = node.seed
+        self.validators = validators
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        return hashlib.sha256(self.serialised).hexdigest()
+
+    @property
+    def serialised(self):
+        return pickle.dumps(self.__dict__)
+
+    def deserialised(self, data):
+        block = self.__class__()
+        block.__dict__ = pickle.loads(data)
+        return block
