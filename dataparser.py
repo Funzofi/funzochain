e26a16bb3e7fe86ac9a3869c2de43673c0ebb2fa
Sushant Shah <sushantshah@cyrocoders.dev>
2022-05-22 19:08:51 +0530

    Created Basic Structure Of Connection Handlers

---
+++ dataparser.py
@@ -0,0 +1,66 @@
+from logging import handlers
+
+
+comm_types = {
+    "conn": [
+        "req",
+        "ack",
+        "drp"
+    ],
+    "blck": [
+        "new",
+        "req",
+        "ack",
+    ],
+    "trnx": [
+        "new",
+        "req",
+        "ack",
+    ]
+}
+
+def parse_data(data):
+    data = data.split(":")
+    if data[0] in comm_types.keys():
+        if data[1] in comm_types[data[0]]:
+            return data[0], data[1]
+        else:
+            return None, None
+    else:
+        return None, None
+
+class connection_handler():
+    def req(self, peer):
+        pass
+
+    def ack(self, peer):
+        pass
+
+    def drp(self, peer):
+        pass
+
+class block_handler():
+    def new(self, peer, block):
+        pass
+
+    def req(self, peer, block):
+        pass
+
+    def ack(self, peer, block):
+        pass
+
+class transaction_handler():
+    def new(self, peer, transaction):
+        pass
+
+    def req(self, peer, transaction):
+        pass
+
+    def ack(self, peer, transaction):
+        pass
+
+handlers = {
+    "conn": connection_handler,
+    "blck": block_handler,
+    "trnx": transaction_handler
+}
