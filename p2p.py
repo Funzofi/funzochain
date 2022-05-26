665cee30739af6cf99facf8fa8f2a85a781a7e7c
Sushant Shah <sushantshah@cyrocoders.dev>
2022-05-26 16:07:19 +0530

    Created Method To Sync Chain

--- p2p.py
+++ p2p.py
@@ -1,3 +1,5 @@
+import os
+from random import shuffle
 from imports import *
 import connection_handler
 
@@ -20,6 +22,38 @@ class p2pInterface:
     def broadcast(self, message):
         for _,sock in self.peerList:
             sock.send(message)
+
+    def sync_chain(self, node):
+        shuffled_nodes = list(self.peerList.keys())
+        shuffle(shuffled_nodes)
+        node_c = 0
+        sync_candidates = {}
+        for peer in shuffled_nodes:
+            if node_c < 3:
+                try:
+                    self.peerList[peer].send("data:int".encode())
+                    remote = node.socket.recv(64)
+                    sync_candidates[peer] = remote
+                except:
+                    self.removePeer(peer)
+            node_c += 1
+        valid_candidates = max(list({rem: [peer for peer in sync_candidates.keys() if sync_candidates[peer] == rem] for rem in sync_candidates.values()}.values()),key=len)
+        progress = 0
+        if "\\" in node.chainFile:
+            os.makedirs(os.path.dirname(node.chainFile), exist_ok=True)
+        with open(node.chainFile, "wb") as f:
+            for peer in valid_candidates:
+                self.peerList[peer].send("data:syn".encode())
+                self.peerList[peer].send(str(progress).encode())
+                while True:
+                    remote = node.socket.recv(64)
+                    print(remote)
+                    f.write(remote)
+                    if len(remote) < 64:
+                        print("Reached end of chain")
+                        node.chain = pickle.load(open(node.chainFile, "rb"))
+                        return
+                    progress += 1
 
     def listen(self, port):
         self.listening = True
