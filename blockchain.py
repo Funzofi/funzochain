8725063c48c5b499412744886c4b0b608862a3cd
Sushant Shah <sushantshah@cyrocoders.dev>
2022-07-13 22:31:29 +0530

    node Object Is Now Stored Within Blockchain

--- blockchain.py
+++ blockchain.py
@@ -3,9 +3,10 @@ from block import Block
 import json
 
 class Blockchain(list):
-    def __init__(self, mainfile, p2pInterface):
+    def __init__(self, node, mainfile, p2pInterface):
         self.chain = list()
         self.index = dict()
+        self.node = node
         self.mainfile = mainfile
         self.p2pInterface = p2pInterface
         self.load()
@@ -19,54 +20,62 @@ class Blockchain(list):
                 raise ValueError("Block already exists")
             self.index[item.calculate_hash()] = len(self.chain)
             self.chain.append(item)
+            print(f"Added block {item.hash[:6]} to chain")
             self.save()
             block_data = item.serialised
             self.p2pInterface.broadcast([
                 b"blck:new",
                 f'{len(block_data):05d}'.encode(),
-                block_data
+                block_data.encode()
             ])
         else:
             raise TypeError("Blockchain can only append blocks")
 
     def save(self):
+        old_file = self.currfile
         block_index = 0
-        for iter_ in range(len(self.chain)):
-            with open(self.currfile, "wb") as f:
-                print(self.currfile)
-                for item in self.chain[block_index:50]:
+        with open(self.currfile, "r") as f:
+            file_size = len(f.readlines())
+        for iter_ in range(int(len(self.chain)/50)+1):
+            with open(self.currfile, "w") as f:
+                for item in self.chain[block_index:(50-file_size)]:
                     f.write(item.serialised)
-                    f.write("\n".encode())
+                    f.write("\n")
                 if len(self.chain) > 50*(iter_+1):
-                    f.write("\n\n".encode())
-                    print(50*(iter_+1))
-                    print((iter_+1)*50)
+                    f.write("\n\n")
                     self.currfile = self.chain[(iter_+1)*50].calculate_hash()
-                    f.write(self.currfile.encode())
-        self.load()
-                
-        self.currfile = self.mainfile
+                    f.write(self.currfile)
+                    file_size = 0
+        if old_file != self.currfile:
+            self.load()
 
     def load(self):
         self.currfile = self.mainfile
         self.index = dict()
-        while True:
-            try:
+        if os.path.exists(self.currfile):
+            print("Hi")
+            while True:
                 chain_lenght = 0
-                with open(self.currfile, "rb") as f:
+                with open(self.currfile, "r") as f:
                     for line in f.readlines():
-                        if line != b"\n":
-                            block = Block(self, "").deserialised(line)
+                        if line != "\n":
+                            print(line)
+                            block = Block(self.node, "").deserialised(line)
+                            print(block.__dict__)
                             self.index[block.hash] = chain_lenght
                             chain_lenght += 1
                             self.chain.append(block)
-                    if f.readline()[-2] == f.readline()[-3] == b"\n":
-                        print(f.readline[-1])
-                        self.currfile = f.readline()[-1].decode()
-                    else:
+                        else:
+                            break
+                    print(f.readlines())
+                    if len(f.readlines()) > 50:
+                        self.currfile = f.readline()[-1]
                         break
-            except:
-                break
+                    else:
+                        return
+        else:
+            print("Bye")
+            open(self.currfile, "w")
             
 
     def __setitem__(self, key, value):
