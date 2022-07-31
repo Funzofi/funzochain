e927a4c152a17de2fbdfdef944dcc6c15f04bb6b
Sushant Shah <sushantshah@cyrocoders.dev>
2022-07-31 13:41:44 +0530

    Seed Root Preprocessing Is Now An Independent Function For Reusability

--- GPoHC.py
+++ GPoHC.py
@@ -31,12 +31,17 @@ class GPoHC():
         for root in self.collect_roots(SOURCE_BLOCKS):
             SEED_ROOT = self.add_by_each_byte(SOURCE_SEED, root)
 
-        SEED_ROOT_PROCESSED = []
-        for char in SEED_ROOT:
-            SEED_ROOT_PROCESSED.extend([int(y) for y in list("".join(format(ord(x), 'b') for x in str(char)))])
+        SEED_ROOT_PROCESSED = self.preprocess_seed_root(SEED_ROOT)
 
         SUPER_SEED = self.model.generator_forward(SEED_ROOT_PROCESSED)
         self.broadcast_super_seed(SUPER_SEED)
+
+    def preprocess_seed_root(self, seed_root):
+        seed_root_processed = []
+        for char in seed_root:
+            seed_root_processed.extend([int(y) for y in list("".join(format(ord(x), 'b') for x in str(char)))])
+        
+        return seed_root_processed
 
     def add_by_each_byte(self, a, b):
         result = ""
